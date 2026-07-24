eab661d1e3f924b58f82ab387d679570625f9f49
Varun <penjcs127@rmkcet.ac.in>
2026-07-24 09:28:59 +0530

    Updated spring week

---
+++ Digital-Nurture-JavaFSE-main/Spring Core_Maven/Exercise-09/src/main/java/com/library/controller/BookController.java
@@ -0,0 +1,46 @@
+package com.library.controller;
+
+import com.library.entity.Book;
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book existing = bookRepository.findById(id).orElse(null);
+        if (existing != null) {
+            existing.setTitle(book.getTitle());
+            existing.setAuthor(book.getAuthor());
+            return bookRepository.save(existing);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
